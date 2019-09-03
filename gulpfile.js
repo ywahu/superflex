ad4ce66dc9923f1ad5d2fd67372021112f1f72c0
Jason Ywahu Morris <jason.morris@travcorp.com>
2019-09-03 16:50:12 -0700

    gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+var del = require('del');
+var browserSync = require('browser-sync').create();
+
 sass.compiler = require('node-sass');
- 
-gulp.task('sass', function () {
+
+gulp.task('clean', function () {
+  return del(['./dist/styles.css'])
+});
+
+gulp.task('sass', gulp.series('clean', function () {
   return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+}));
+
+gulp.task('watch', function () {
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+
+gulp.task('serve', gulp.parallel('sass', function () {
+
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+
+}));
